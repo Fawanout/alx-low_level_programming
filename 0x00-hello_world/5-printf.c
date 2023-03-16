0e0e42c752ca4ad73b7783a61612aaa7df16c0fb
fawanout <f.wanir^gmail.com>
2023-03-16 07:54:55 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* main a program that print aline using the printf function*/
+/* Return value of main (0) means code running without error*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
