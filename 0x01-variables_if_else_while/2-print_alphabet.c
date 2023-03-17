943854168ba836a427efd252e0e3cfb43dd0a776
fawanout <f.wanir^gmail.com>
2023-03-17 07:14:03 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function to run the programe
+ * Return: 0 (succes)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
