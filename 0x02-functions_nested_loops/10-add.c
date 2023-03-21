431658b15b847ad8eead07ba6f7bbcc95d2d1b42
fawanout <f.wanir^gmail.com>
2023-03-21 15:17:04 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that add 2 integers
+ * @num1 : parameter for addition op
+ * @num2 : parameter to add
+ * Return: the result
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
