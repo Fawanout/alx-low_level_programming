d50776b98e78668c8552607f37dc962da56ddefb
fawanout <f.wanir^gmail.com>
2023-03-23 08:51:39 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints squares
+ * @size: input
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+		else
+		{
+			for (row = 0; row < size; row++)
+			{
+				for (column = 0; column < size; column++)
+				{
+					_putchar(35);
+				}
+				_putchar('\n');
+			}
+		}
+}
