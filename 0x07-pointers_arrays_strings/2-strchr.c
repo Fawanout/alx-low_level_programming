50700c06ecf8a60b0d4d92a8d67eac196f6de05e
“Username” <f.wanir@gmail.com>
2023-04-03 06:29:09 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s:
+ * @c:
+ * Return: always 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
