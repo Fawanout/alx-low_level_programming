8b29a4d4c73e99065650970919f34ab69948f901
“Username” <f.wanir@gmail.com>
2023-04-03 10:54:12 -0700

    6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: input
+ * @needle: input
+ * Return: returns a pointer or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *p, *pp;
+
+		while (needle[i] <= 
+		{
+			while (
+
+}
