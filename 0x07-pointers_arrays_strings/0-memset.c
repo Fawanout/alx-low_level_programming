b035f71ac688ef2d3252b4d73c5dc8de6d132178
“Username” <f.wanir@gmail.com>
2023-04-03 05:40:35 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * char *_memset - fills memory with a constant byte
+ * @s: pointer
+ * @b: constant byte
+ * @n: bytes
+ * Return: pointer to a memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0;i < n; i++)
+		s[i] = b;
+	return (s);
+}
