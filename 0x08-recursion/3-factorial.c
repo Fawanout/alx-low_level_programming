2c46183500f8e5b6874d12607490a956341678d9
“Username” <f.wanir@gmail.com>
2023-04-05 05:54:32 -0700

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - returns the fact of a given num
+ * @n: input
+ * Return: fact
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+		return (n * factorial(n - 1));
+}
