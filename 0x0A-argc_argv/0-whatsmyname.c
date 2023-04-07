8a57a3410587ac53d482c17f812945b25d2050f5
“Username” <f.wanir@gmail.com>
2023-04-07 09:07:42 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+
+/**
+ * main - prints program name
+ * @argv: input
+ * @argc:input
+ * Return: value
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
