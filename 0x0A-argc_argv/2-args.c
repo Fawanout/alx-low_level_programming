3bc49f12f69171e4422d1f0c4864a9a8b8cb6deb
“Username” <f.wanir@gmail.com>
2023-04-10 07:06:31 -0700

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all the args that the prog recieves
+ * @argc:input
+ * @argv:input
+ * Return:always 0 success
+ */
+
+int main(int argc, char const *argv[])
+{
+	int = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
