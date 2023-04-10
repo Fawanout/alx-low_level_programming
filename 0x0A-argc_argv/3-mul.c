30c85c3b697747e2a34511ccd8867a21ac43d9ad
“Username” <f.wanir@gmail.com>
2023-04-10 07:20:38 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argv:number of args
+ * @argc:pointer to array
+ * Return: always 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int pr;
+
+	if (argc == 3)
+	{
+		pr = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", pr);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+	return (0);
+}
+
