56ff0d62c6508e4c4f096cddcdc0e668a285aa1f
“Username” <f.wanir@gmail.com>
2023-04-12 13:36:00 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strdup - returns a pointer to  a newly allocated space in memory
+ * @str: string
+ * Return: 0 success
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int size = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size] != '\0'; size++)
+		;
+	m = malloc(size * sizeof(str) + 1);
+
+	if (s == 0)
+		return (NULL);
+	else if
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (s);
+}
